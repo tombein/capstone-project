6ed8c59997bbd653d049fbbd14f76db872dcc4a2
Tom Bein <tom.bein@hotmail.de>
2022-06-05 22:28:49 +0200

    add homepage image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
-import StyledMain from '../src/components/styled-components/StyledMain';
+import StyledLanding from '../src/components/styled-components/StyledLanding';
+import Image from 'next/image';
 
 export default function Home() {
 	return (
-		<StyledMain>
-			<p>Homepage</p>
-		</StyledMain>
+		<main>
+			<StyledLanding>
+				<Image src="/logo.png" alt="logo" width={400} height={400} />
+			</StyledLanding>
+		</main>
 	);
 }
---
+++ src/components/styled-components/StyledLanding.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const StyledLanding = styled.section`
+	background-color: var(--main-color);
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+`;
+
+export default StyledLanding;
